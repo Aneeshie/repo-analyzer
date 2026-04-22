83ff3d4d6ec761610c97c6e55767d43b9f3bdd1b
Aneesh <aneeshdas556@gmail.com>
2026-04-22 18:20:46 +0530

    Rename test.yml to test-migrations.yml

--- infra/scripts/setup.sh
+++ infra/scripts/setup.sh
@@ -5,18 +5,10 @@ set -e
 cd "$(dirname "$0")/../docker"
 docker-compose up -d
 
-<<<<<<< HEAD
-echo "Waiting for PostgreSQL..."
-=======
 echo "Waiting for PostgreSQL.."
->>>>>>> bb2c496 (feat(scripts): add setup and teardown scripts)
 sleep 5
 
 cd ../..
 migrate -path infra/migrations -database "postgres://repo_user:repo_pass@localhost:5432/repo_analyzer?sslmode=disable" up
 
-<<<<<<< HEAD
 echo "PostgreSQL ready and migrations applied!!"
-=======
-echo "PostgreSQL ready and migrations applied"
->>>>>>> bb2c496 (feat(scripts): add setup and teardown scripts)
