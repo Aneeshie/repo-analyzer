398bae44ca4d13e39962c8f2672bbff49969ffde
Aneeshie <aneeshdas556@gmail.com>
2026-05-02 11:13:33 +0530

    feat: setup shadcn

--- apps/web/app/page.tsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const Home = () => {
-  return <div className="text-4xl font-bold text-blue-500">Home</div>;
-};
-
-export default Home;
---
+++ apps/web/src/app/page.tsx
@@ -0,0 +1,10 @@
+import { Button } from "@/components/ui/button";
+
+export default function Home() {
+  return (
+    <>
+      <div className="text-4xl text-blue-500">hello world</div>
+      <Button>Hello</Button>
+    </>
+  );
+}
